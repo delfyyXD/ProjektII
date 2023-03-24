73ee045a61d7a1936f94017fcc61a8c8e9884ae2
delfyyXD <delfanisgod420@gmail.com>
2023-03-24 18:41:08 +0100

    init

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-//NOTHING
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello<pr>\n";
+        echo "Date is: " . date('j-m-y, h:i:s');
+    ?>
+</body>
+</html>
