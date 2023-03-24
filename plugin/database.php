0d8798f4042abf14c5c34d33f833004b19c7c57c
delfyyXD <delfanisgod420@gmail.com>
2023-03-24 21:07:32 +0100

    init

---
+++ plugin/database.php
@@ -0,0 +1,12 @@
+<?php
+$db_host = 'localhost';
+$db_name = 'willimsuvsyndrom';
+$db_user = 'root';
+$db_pass = '';
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if($mysqli->connect_error) {
+    printf("Pripojeni selhalo %s\n", $mysqli->connect_error);
+    exit();
+}
