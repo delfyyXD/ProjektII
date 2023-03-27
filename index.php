53c7e268d1bb16c002ab96c69c2122d6ff7c4ada
delfyyXD <delfanisgod420@gmail.com>
2023-03-27 21:47:31 +0200

    init

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Start</title>
+    <title>Uvodni web</title>
 </head>
 <body>
     <?php
-        echo "Hello hello<pr>\n";
-        echo "Date is: " . date('j-m-y, h:i:s');
+        echo "PROJEKT II<br>";
+        echo "<br>Date is: " . date('j-m-y, h:i:s');
     ?>
 </body>
 </html>
